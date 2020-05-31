b788ced3c6888206c432b5eabb90ca2fd00bbf11
Nitin Sapru <sapru-nitin@outlook.com>
2020-05-31 19:31:59 +0530

    Add project files.

---
+++ DockerSample.BeanStalkWorker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace DockerSample.BeanStalkWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i<=10; i++)
+            {
+                Console.WriteLine($"This is the {i}th iteration.");
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
